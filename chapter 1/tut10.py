82b68abde8a2995ba2288425c0bf2609f595afea
Priyankawadile29 <154313954+Priyankawadile29@users.noreply.github.com>
2024-06-15 18:37:57 +0530

    Delete chapter 1 directory

--- chapter 1/tut10.py
+++
@@ -1,9 +0,0 @@
-#dictonary is nothimg but key value pairs
-d1 = {}
-#print (type(d1))
-d2 = { "hemant":"burger","piu":"eggs", "om":"colddrinks"}
-#print (d2["piu"])
-#del d2["piu"]
-#print(d2)
-d2.update({"khushi":"chai"})
-print(d2)
